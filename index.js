404447435e6a9f1fa0d6dffb284beeba6ae25641
Sergio Marrocoli <s.marrocoli@gmail.com>
2019-04-13 12:52:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  let template = document.getElementById("recipe-form-template").innerHTML;
+  document.getElementsByTagName("main")[0].innerHTML += template;
 }
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
