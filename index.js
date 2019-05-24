e648c137a98e37c8bbc318ef3a439253bad2d4e1
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-24 20:14:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  Handlebars.registerHelper('displayIngredient', function() {
-      return new Handlebars.SafeString("<li name=\"ingredients\">" + this.value + "</li>")
+  Handlebars.registerHelper('displayIngredient', function(ing) {
+      return new Handlebars.SafeString("<li name=\"ingredients\">" + ing + "</li>")
   })
-  Handlebars.registerPartial('recipeDetailsPartial', '{{description}}');
-  Handlebars.registerPartial('recipeDetailsPartial', '{{ingredients}}');
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
   let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
   let name = '';
   let description = '';
   let ingredients = ['', '', '', '', ''];
-  let result = template(name, description, ingredients);
-  document.getElementsByTagName("main")[0].innerHTML += result;
+  let result = template({name: name, description: description, ingredients: ingredients});
+  document.getElementsByTagName("main")[0].innerHTML = result;
 }
 
 function handleSubmit() {
-  event.preventDefault();
   let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
-  let name = document.getElementsByName('name')[0].value;
-  let description = document.getElementsByName('description')[0].value;
-  let ingredients = document.getElementsByName('ingredients');
-  let result = template(name, description, ingredients);
+  let name = document.getElementById('name').value;
+  let description = document.getElementById('description').value;
+  let ingredients = [...document.getElementsByName('ingredients')].map(el => el.value);
+  let result = template({name: name, description: description, ingredients: ingredients});
   document.getElementsByTagName("main")[0].innerHTML = result;
 }
 
 function displayEditForm() {
   let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
-  let name = document.getElementsById('name')[0].value;
-  let description = document.getElementsById('description')[0].value;
-  let ingredients = document.getElementsByName('ingredients');
-  let result = template(name, description, ingredients);
+  let name = document.getElementById('name').innerHTML;
+  let description = document.getElementById('description').innerHTML;
+  let ingredients = [...document.getElementsByName('ingredients')].map(el => el.innerHTML);
+  let result = template({name: name, description: description, ingredients: ingredients});
   document.getElementsByTagName("main")[0].innerHTML = result;
 }
 document.addEventListener("DOMContentLoaded", function(event) {
